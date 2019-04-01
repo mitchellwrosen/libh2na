4cfb11ab65c387c35777996b90833fb006f0f21c
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-03-31 23:40:45 -0400

    Hash committed value

--- src/H2NA/Commitment.hs
+++ src/H2NA/Commitment.hs
@@ -4,9 +4,11 @@ module H2NA.Commitment
   , committed
   ) where
 
+import Crypto.Hash.Algorithms (Blake2s_256)
 import Crypto.Number.Serialize (os2ip)
 import Data.ByteString         (ByteString)
 
+import qualified Crypto.Hash as Hash
 import qualified Pedersen
 
 
@@ -18,13 +20,16 @@ data Commitment
   = Commitment Pedersen.ECCommitParams Pedersen.ECCommitment Integer
 
 -- | Commit to a secret value.
+--
+-- This function internally hashes the secret value first, so it may be
+-- arbitrarily long.
 commit :: ByteString -> IO Commitment
 commit value = do
   params :: Pedersen.ECCommitParams <-
     Pedersen.ecSetup Nothing
 
   Pedersen.ECPedersen commitment reveal_ <-
-    Pedersen.ecCommit (os2ip value) params
+    Pedersen.ecCommit (hash value) params
 
   pure (Commitment params commitment (Pedersen.ecRevealScalar reveal_))
 
@@ -35,6 +40,10 @@ committed (Commitment params commitment scalar) value =
     params
     commitment
     Pedersen.ECReveal
-      { Pedersen.ecRevealVal = os2ip value
+      { Pedersen.ecRevealVal = hash value
       , Pedersen.ecRevealScalar = scalar
       }
+
+hash :: ByteString -> Integer
+hash =
+  os2ip . Hash.hash @_ @Blake2s_256
